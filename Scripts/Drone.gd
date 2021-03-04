2c2d5ba74205014775c26097f5617e463ad1e997
tristantyler <tristantyler8@gmail.com>
2021-03-04 14:07:44 -0600

    Fixed the tower d stuff

--- Scripts/Drone.gd
+++ Scripts/Drone.gd
@@ -1,19 +1,16 @@
 extends PathFollow2D
 
-var speed = 60;
-var hp = 10
-
+export var speed = 40
+export var hp = 10
 
 func _physics_process(delta):
 	offset += speed * delta
-	
 	if unit_offset >= 1:
 		reached_end()
 
-
 func reached_end():
 	queue_free()
-
+	
 
 func _on_Area2D_area_entered(area):
 	if area.is_in_group("shot"):
--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-var toer = load("res://Scenes/Tower.tscn")
+var tower = load("res://Scenes/Tower.tscn")
 var mob = load("res://Scenes/Drone.tscn")
 var instance
 
@@ -10,39 +9,31 @@ var building = false
 var cash = 50
 var wave = 0
 var mobs_left = 0
-var wave_mobs = [5, 15, 30]
-
+var wave_mobs = [5, 15, 30, 60, 120]
 
 func _ready():
-	$WaveTime.start()
+	$WaveTimer.start()
 
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	$CashLabel.text = "cash: " + str(cash)
-
-
-func _on_TextureButton_pressed():
-	if !building and cash >= 25:
-		instance = tower.instance()
-		add_child(instance)
-		building = true
-
 
 func tower_built():
 	building = false
-	cash -= 25
-
+	cash -=25
+	print("built tower")
 
 func add_cash(num):
 	cash += num
 
-
 func _on_WaveTimer_timeout():
+#	print("Wave Start")
 	mobs_left = wave_mobs[wave]
 	$MobTimer.start()
+	$WaveTimer.stop()
 
 
 func _on_MobTimer_timeout():
+#	print("Spawning")
 	instance = mob.instance()
 	$Path2D.add_child(instance)
 	mobs_left -= 1
@@ -51,3 +42,11 @@ func _on_MobTimer_timeout():
 		wave += 1
 		if wave < len(wave_mobs):
 			$WaveTimer.start()
+
+
+func _on_TextureButton_pressed():
+	if !building and cash >= 25:
+		print("build selected")
+		instance = tower.instance()
+		add_child(instance)
+		building = true
