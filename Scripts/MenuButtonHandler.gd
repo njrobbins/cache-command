128c31a063c93bfaad1f869ac256bb2c4673394f
njrobbins <njrobbins@protonmail.com>
2021-03-03 19:52:04 -0600

    Modified and added to layout

--- Scripts/MenuButtonHandler.gd
+++ Scripts/MenuButtonHandler.gd
@@ -2,23 +2,44 @@ extends Control
 
 var scene
 
-func _on_StartButton_pressed():
-	scene = get_tree().change_scene("res://Scenes/DigDugMain.tscn")
-
-func _on_Instructions_pressed():
-	scene = get_tree().change_scene("res://Scenes/Instructions.tscn")
-
-func _on_Credits_pressed():
-	scene = get_tree().change_scene("res://Scenes/Credits.tscn")
-
-
 func _on_BackButton_pressed():
 	scene = get_tree().change_scene("res://Scenes/MainMenu.tscn")
 
+# func _on_ContinueButton_pressed():
+	# TODO
 
-func _on_BackToMenuButton_pressed():
+func _on_CreditsButton_pressed():
+	scene = get_tree().change_scene("res://Scenes/Credits.tscn")
+
+func _on_InstructionsButton_pressed():
+	scene = get_tree().change_scene("res://Scenes/Instructions.tscn")
+
+func _on_LoseLevelButton_pressed():
+	scene = get_tree().change_scene("res://Scenes/LevelLost.tscn")
+
+func _on_MainMenuButton_pressed():
 	scene = get_tree().change_scene("res://Scenes/MainMenu.tscn")
 
+func _on_PauseMenuButton_pressed():
+	scene = get_tree().change_scene("res://Scenes/PauseMenu.tscn")
+
+# func _on_QuitButton_pressed():
+	# TODO
+
+# func _on_ResumeButton_pressed():
+	# TODO
+
+func _on_StartButton_pressed():
+	scene = get_tree().change_scene("res://Scenes/SiliconMiningMain.tscn")
 
 func _on_TDButton_pressed():
 	scene = get_tree().change_scene("res://Scenes/TowerDefenseMain.tscn")
+
+# func _on_TowerShopButton_pressed():
+	# TODO
+
+# func _on_TryAgainButton_pressed():
+	# TODO
+
+func _on_WinLevelButton_pressed():
+	scene = get_tree().change_scene("res://Scenes/LevelWon.tscn")
