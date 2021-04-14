c3e6c18d5e8c70a5ad10755be1b5ea920c334278
tristantyler <52974257+tristantyler@users.noreply.github.com>
2021-04-13 21:50:04 -0500

    Updated DD collisions

--- Scripts/DigDugGame.gd
+++ Scripts/DigDugGame.gd
@@ -55,32 +55,6 @@ func _ready():
 			inst.position = Vector2(x * 64, y * 64)
 			$Map.add_child(inst)
 			number_of_enemies -= 1
-	
-	
-#	rng.randomize()
-#	var size_w = ProjectSettings.get_setting("display/window/size/width")
-#	var size_h = ProjectSettings.get_setting("display/window/size/height")
-#	var tiles_w = (size_w / cell_w) + 1
-#	var tiles_h = (size_h / cell_h) + 1
-#	var random_seed = rng.randf_range(-99999.0, 99999.0)
-#	var grid = Game.MapGen.getMap(tiles_w, tiles_h, random_seed, 47)
-#	var inst
-#	for y in range(tiles_h):
-#		for x in range(tiles_w):
-#			if grid[y][x]:
-#				if randi() % 20 == 0:
-#					inst = Res.instance()
-#				else:
-#					inst = Tile.instance()
-#				inst.position = Vector2(x * cell_w, y * cell_h)
-#				$Map.add_child(inst)
-#			else:
-#				if !(x >= 0 && x < 3) && !(y >= 0 && y < 3) :
-#					if randi() % 8 == 0 and number_of_enemies > 0:
-#						inst = Enemy.instance()
-#						inst.position = Vector2(x * cell_w, y * cell_h)
-#						$Map.add_child(inst)
-#						number_of_enemies -= 1
 
 
 func _process(_delta):
